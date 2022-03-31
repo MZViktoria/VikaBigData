e41a3716466aeacdb8fbd5d05a1e6bac486e6c77
MZViktoria <99808502+MZViktoria@users.noreply.github.com>
2022-03-31 17:18:59 +1000

    add hw4

---
+++ Test4/test_task2.py
@@ -0,0 +1,42 @@
+import os
+
+import pytest
+
+import HW4.Task2
+
+
+@pytest.mark.parametrize(["data"],
+                         [
+                             [1.5], [2.0], [2.5], [1.0], [1.2], [2.4], [1.7]
+                         ])
+def test_positive(data):
+    with open("data.txt", "w+") as file:
+        file.write(str(data))
+    assert HW4.Task2.read_magic_number("data.txt")
+    os.remove("data.txt") # method in Python is used to remove or delete a file path.
+    assert not os.path.exists("data.txt") #checks for the existence of a file
+
+@pytest.mark.parametrize(["data"],
+                         [
+                             [7.5], [22.5], [14.2]
+                         ])
+def test_negative(data):
+    with open("data.txt", "w+") as file:
+        file.write(str(data))
+    assert not HW4.Task2.read_magic_number("data.txt")
+    os.remove("data.txt")
+    assert not os.path.exists("data.txt")
+
+@pytest.mark.parametrize(["data"],
+                         [
+                             ["2.5"], [(1, 0)], [{"key1": "value1"}]
+                         ])
+def test_error(data):
+    try:
+        with open("data.txt", "w+") as file:
+            file.write(str(data))
+        HW4.Task2.read_magic_number("data.txt")
+    except ValueError:
+        assert ValueError
+    os.remove("data.txt")
+    assert not os.path.exists("data.txt")

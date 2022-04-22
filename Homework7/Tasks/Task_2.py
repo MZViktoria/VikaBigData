829666a59849d56cb8aff854e6b277c393028c25
MZViktoria <99808502+MZViktoria@users.noreply.github.com>
2022-04-22 20:41:08 +1000

    Home work7

---
+++ Homework7/Tasks/Task_2.py
@@ -0,0 +1,46 @@
+"""
+Given two strings. Return if they are equal when both are typed into
+empty text editors. # means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Examples:
+    Input: s = "ab#c", t = "ad#c"
+    Output: True
+    # Both s and t become "ac".
+
+    Input: s = "a##c", t = "#a#c"
+    Output: True
+    Explanation: Both s and t become "c".
+
+    Input: a = "a#c", t = "b"
+    Output: False
+    Explanation: s becomes "c" while t becomes "b".
+
+"""
+
+
+def backspace_compare(first: str, second: str):
+    def backspace_method(s: str):
+        i = 0
+        while i < len(s):
+            if s[i] == '#': #проверка на #
+                if i == 0:
+                    s = s[1:] # берём все элементы кроме 1
+                else:
+                    s = s[:i - 1] + s[i + 1:] # берём все элементы слева и справа со сдвигом
+                    i = 0
+            i += 1
+        return s
+
+    new_first = backspace_method(first)
+    new_second = backspace_method(second)
+    if new_first == new_second:
+        return f"Output: True\nExplanation: both s and t become '{new_first}'"
+    else:
+        return f"Output: False\nExplanation: s becomes '{new_first}' " \
+               f"while t becomes '{new_second}'"
+
+str1=input("введите строку 1: ")
+str2=input("введите строку 2: ")
+print(backspace_compare(str1, str2))
---
+++ Homework7/Tasks/task_1.py
@@ -0,0 +1,47 @@
+"""
+Given a dictionary (tree), that can contains multiple nested structures.
+Write a function, that takes element and finds the number of occurrences
+of this element in the tree.
+
+Tree can only contains basic structures like:
+    str, list, tuple, dict, set, int, bool
+"""
+from typing import Any
+
+
+# Example tree:
+example_tree = {
+    "first": ["RED", "BLUE"],
+    "second": {
+        "simple_key": ["simple", "list", "of", "RED", "valued"],
+    },
+    "third": {
+        "abc": "BLUE",
+        "jhl": "RED",
+        "complex_key": {
+            "key1": "value1",
+            "key2": "RED",
+            "key3": ["a", "lot", "of", "values", {"nested_key": "RED"}],
+        }
+     },
+    "fourth": "RED",
+}
+
+
+def find_occurrences(tree: dict, element: Any) -> int:
+    #return str(tree.values()).count(str(element))
+    def counter(mnogestvo, elem):
+        for val in (mnogestvo.values() if isinstance(mnogestvo,
+                                                dict) else mnogestvo):  #мы проверяем тип данных.явлияется ли элмент словарём или чем-то другим
+            if val == elem:
+                yield val
+            elif isinstance(val, (list, tuple, set)):
+                yield from counter(val, elem)
+            elif isinstance(val, dict):
+                yield from counter(val.values(), elem)
+    return len(list(counter(tree, element)))
+
+
+if __name__ == '__main__':
+    print(example_tree.items())
+    print(find_occurrences(example_tree, "RED"))  # 6
